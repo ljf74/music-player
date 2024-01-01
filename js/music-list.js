6039d33375320397267cf9f9a2414b43bb609c10
ljf74 <136962272+ljf74@users.noreply.github.com>
2024-01-01 08:35:00 +0000

    Initial commit

---
+++ js/music-list.js
@@ -0,0 +1,55 @@
+// To add more song, just copy the following code and paste inside the array
+//   {
+//     name: "Here is the music name",
+//     artist: "Here is the artist name",
+//     img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+//     src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+//   }
+//paste it inside the array as more as you want music then you don't need to do any other thing
+
+
+let allMusic = [
+  {
+    name: "鸭梨大",
+    artist: "至上励合",
+    img: "鸭梨大",
+    src: "鸭梨大"
+  },
+  {
+    name: "超级风格",
+    artist: "SpeXial",
+    img: "超级风格",
+    src: "超级风格"
+  },
+  {
+    name: "身姿",
+    artist: "Jay Park",
+    img: "身姿",
+    src: "身姿"
+  },
+  // {
+  //   name: "Hardwind - Want Me",
+  //   artist: "Mike Archangelo",
+  //   img: "music-4",
+  //   src: "music-4"
+  // },
+  // {
+  //   name: "Jim - Sun Goes Down",
+  //   artist: "Jim Yosef x Roy",
+  //   img: "music-5",
+  //   src: "music-5"
+  // },
+  // {
+  //   name: "Lost Sky - Vision NCS",
+  //   artist: "NCS Release",
+  //   img: "music-6",
+  //   src: "music-6"
+  // },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];
